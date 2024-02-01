0546bf819326ff6d95774f9400f245fbb53e3eb8
loszngol <jakub.jarosz@skiff.com>
2024-02-01 17:20:26 +0100

    initial code

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+clear
+
+echo "-------------------------------"
+echo "Welcome to gentoo installer :)"
+echo "-------------------------------"
+
+ping -qc 3 gentoo.org > /dev/null
+
+if [ $? -eq 2 ]; then
+    echo "Network not working"
+else
+    echo "Network working"
+fi
+
+clear
+lsblk
+
+echo "Which disk do you want to partition"
+read disk
+
+cfdisk $disk
+
+echo "Which partition wo you want to farmat as fat?"
+read fat_part
+
+echo "Which partition do you want to format as ext4?"
+read root_part
+
+read -p "Do you want to use swap? (yes/no)" answer
+
+if [ "$answer" = "y" -o "$answer" = "Y" ]; then
+    echo "Which partition do you want to format as swap?"
+    read swap_part
+    swapon $swap_part
+
+mkfs.vfat -F 32 $fat_part
+mkfs.ext4 $root_part
+
+mkdir --parents /mnt/gentoo
+mount $root_part /mnt/gentoo
+
+cd /mnt/gentoo
+links "https://gentoo.org/downloads/"
+
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
